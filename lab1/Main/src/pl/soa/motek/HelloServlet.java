1750b69ab873190461ba8f3e522439d05ab755e5
tomaboro <motek@student.agh.edu.pl>
2018-03-27 14:50:21 +0200

    init commit v2

---
+++ lab1/Main/src/pl/soa/motek/HelloServlet.java
@@ -0,0 +1,24 @@
+package pl.soa.motek;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("Hello World !");
+        out.close();
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException,IOException {
+        doGet(request,response);
+    }
+}
